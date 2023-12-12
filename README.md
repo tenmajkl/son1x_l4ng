fa85356ba01499d930443c12c095850931d71322
TENMAJKL <silder.hologram@gmail.com>
2023-12-12 23:58:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# sonixxxx_l>ng
+
+is a web programming languages that uses hyper characters to generate tones and musixx
+
+## syntax
+
+each audio channel is determined using >><< 
+
+meaning that everything inside >><< is in channel mode 
+
+Aplitude is number inside \_\_
+
+Color of sound is designed in // where first character defines synthesis mode
+
+### subtractive synth
+
+- is defined using - symbol
+- then its followed by waveform-defining symbols: ~ (sine) ^ (triangle) \ (saw) _ (square)
+- the number of symbols determines number of voices
+- inside !! is detune value
+- inside \*\* is cutoff value
+- inside @@ is resonance value
+- when no envelope is present, it drones
---
+++ lang.js
@@ -0,0 +1,176 @@
+const re = /(?<channel_open>\>\>)|(?<channel_close>\<\<)|(?<symbol>[~|\^|\\\|_|\/|!|\*|@\-])|(?<number>[0-9]+)/g
+
+function lex(code) {
+    const tokens = [];
+    let match;
+    while (match = re.exec(code)) {
+        const { groups } = match;
+        if (groups.channel_open) {
+            tokens.push({
+                type: 'channel_open',
+                value: groups.channel_open,
+            })
+        } else if (groups.channel_close) {
+            tokens.push({
+                type: 'channel_close',
+                value: groups.channel_close,
+            })
+        } else if (groups.symbol) {
+            tokens.push({
+                type: 'symbol',
+                value: groups.symbol,
+            })
+        } else if (groups.number) {
+            tokens.push({
+                type: 'number',
+                value: groups.number,
+            })
+        } else {
+            return null;
+        }
+    }
+
+    return tokens;
+}
+
+function parse(tokens) {
+    let index = 0;
+    let result = [];
+    while (index < tokens.length) {
+        switch (tokens[index].type) {
+            case 'channel_open':
+                index++;
+                let channel = parseChannel(tokens, index);
+                if (channel.error !== undefined) {
+                    return channel;
+                }
+                result.push(channel.channel);
+                index = channel.index;
+                break;
+            default: 
+                return null;
+        }
+        index++;
+    }
+
+    return result;
+}
+
+const types = {
+    '-': ['subtractive', parseSubtractiveSynthColor],
+};
+
+function parseChannel(tokens, index) {
+    if (tokens[index].type !== 'symbol') {
+        return {
+            error: 'Expected symbol that determines channel type',
+        };
+    }
+    let channel = {
+    }; 
+    let type = types[tokens[index].value];
+    if (type == null) {
+        return {
+            error: 'Expected symbol that determines channel type',
+        }
+    }
+    index++;
+    channel.type = type[0];
+
+    while (tokens[index].type !== 'channel_close') {
+        let token = tokens[index];
+        if (token.value == '/') {
+            index++;
+            let color = type[1](tokens, index);
+            if (color.error === undefined) {
+                return color;
+            }
+
+            channel.color = color.color;
+            index = color.index;
+        }
+    }
+
+    return {
+        channel: channel,
+        index: index,
+    }
+}
+
+function parseNumericValue(determiner, tokens, index) {
+    if (tokens[index] !== determiner) {
+        return null;
+    }
+
+    index++;
+    let result = tokens[index];
+    if (result.type !== 'number') {
+        return {
+            error: 'Expected number, got ' + result.type
+        };
+    }
+    index++;
+    if (result.value !== determiner) {
+        return {
+            error: 'Unclosed ' + determiner
+        };
+    }
+    return {
+        value: result.value,
+        determiner: determiner,
+        index: index,
+    }
+}
+
+const waveforms = {
+    '~': 0,
+    '^': 1, 
+    '\\': 2,
+    '_': 3,
+};
+
+function parseSubtractiveSynthColor(tokens, index) {
+    let color = {
+        waveforms: [],
+        detune: 0,
+        cutoff: 100,
+        resonance: 0,
+    }
+    while (tokens[index].value !== '/') {
+        let value = parseNumericValue('!', tokens, index) 
+                    || parseNumericValue('*', tokens, index) 
+                    || parseNumericValue('@', tokens, index)
+        ;
+
+        if (value !== null) {
+            if (value.error !== undefined) {
+                return value;
+            }
+            switch (value.determiner) {
+                case '!': color.detune = value.value; break;
+                case '*': color.cutoff = value.value; break;
+                case '@': color.resonance = value.value; break;
+            }
+            index = value.index;
+            continue;
+        }
+
+        if (waveforms[tokens[index].value] !== undefined) {
+            color.waveforms.push(tokens[index].value);
+            index++;
+            continue;
+        }
+
+        return {
+            error: 'unexpected token',
+        }
+    }
+    index++;
+
+    return {
+        color: color,
+        index: index,
+    };
+}
+
+console.log(parse(lex('>>-_^_<<'), 0));
